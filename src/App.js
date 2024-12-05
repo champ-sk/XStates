a1139ec1c7bb0a7f55439a7de59d7482eba60004
champ-sk <shubhamkumar3183@gmail.com>
2024-12-05 13:48:03 +0530

    adding few changes

--- src/App.js
+++ src/App.js
@@ -1,23 +1,52 @@
-import logo from './logo.svg';
 import './App.css';
+import {useEffect} from "react";
+import { useState  } from 'react';
 
 function App() {
+
+  const [countries, setCountries] = useState([]);
+  const [states, setStates] = useState([]);
+  const [cities, setCities] = useState([]);
+
+
+  const [selCountry, setSelCountry] =useState("");
+  const [selState, setSelState] =useState("");
+  const [selCity, setSelCity] =useState("");
+  useEffect(
+    ()=>{
+fetch("https://crio-location-selector.onrender.com/countries").then(res=>res.json()).then(data=>setCountries(data));
+    },[]
+  );
+
+  useEffect(
+    ()=>{
+fetch(`https://crio-location-selector.onrender.com/country=${selCountry}/states`).then(res=>res.json()).then(data=>setStates(data));
+    },[selCountry]
+  );
+
+  useEffect(
+    ()=>{
+fetch(`https://crio-location-selector.onrender.com/country=${selCountry}/state=${selState}/cities`).then(res=>res.json()).then(data=>setCities(data));
+    },[selCountry, selState]
+  );
+  console.log(countries);
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+     <select name="country" id="country" value={selCountry} onChange={(e)=>setSelCountry(e.target.value)} >
+{countries.map((country)=>(<option >{country}</option>))}
+     </select>
+
+     <select name="country" id="country" value={selState} onChange={(e)=>setSelState(e.target.value)} >
+      <option>Select State</option>
+{states.map((country)=>(<option >{country}</option>))}
+     </select>
+
+     <select name="country" id="country" value={selCity} onChange={(e)=>setSelCity(e.target.value)} >
+      <option>Select City</option>
+{cities.map((country)=>(<option >{country}</option>))}
+     </select>
+
+    <h1>{`You Selected ${selCity}, ${selState}, ${selCountry}`}</h1>
     </div>
   );
 }
